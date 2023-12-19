ae46a5498b690c7af24afc0b30c85e5a219d7f4b
Youri van Mill <me@youri.dev>
2023-12-19 10:39:58 +0100

    13123123123123

--- src/plugin/generateTemplate.ts
+++ src/plugin/generateTemplate.ts
@@ -58,9 +58,9 @@ function renderPreloadLink(file: string) {
 
 function renderCssForSsr(mods: Set<ModuleNode>, styles = new Map<string, string>(), checkedComponents = new Set()) {
   for (const mod of mods) {
-    if ((mod.file?.endsWith(".scss")
-        || mod.file?.endsWith(".css")
-        || mod.id?.includes("vue&type=style")) &&
+    if ((mod.file?.endsWith('.scss')
+        || mod.file?.endsWith('.css')
+        || mod.id?.includes('vue&type=style')) &&
       mod.ssrModule
     ) {
       styles.set(mod.id, mod.ssrModule.default)
