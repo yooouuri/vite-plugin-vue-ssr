425736e1afd1324f039d647182334cd5d62b458e
Youri van Mill <me@youri.dev>
2024-01-22 10:48:21 +0100

    refactor(example): load files on server start, add compression

--- example/server.js
+++ example/server.js
@@ -10,18 +10,19 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const app = express()
 
+const main = (await import(resolve(__dirname, './dist/server/main.js'))).default
+
+const template = readFileSync(resolve('dist/client/index.html'), 'utf-8')
+
+const manifest = JSON.parse(
+  readFileSync(resolve('dist/client/.vite/ssr-manifest.json'), 'utf-8')
+)
+
+app.use((await import('compression')).default())
 app.use(cookieParser())
 app.use('/', serveStatic(resolve('dist/client'), { index: false }))
 app.use('*', async (req, res) => {
   const url = req.originalUrl
-
-  const main = (await import(resolve(__dirname, './dist/server/main.js'))).default
-
-  const template = readFileSync(resolve('dist/client/index.html'), 'utf-8')
-
-  const manifest = JSON.parse(
-    readFileSync(resolve('dist/client/.vite/ssr-manifest.json'), 'utf-8')
-  )
 
   const { html, redirect } = await generateTemplate(main, url, template, req, res, manifest)
 
