dddd6b02a1fb558f410c52c1d7d40192ab5b1a5e
Youri van Mill <me@youri.dev>
2024-06-25 14:05:35 +0200

    implement every html alteration
    restructure generateHtml function

--- src/plugin/generateHtml.ts
+++ src/plugin/generateHtml.ts
@@ -1,145 +1,86 @@
-import type { SSRContext } from 'vue/server-renderer'
-import { parse } from 'node-html-parser'
-import { ModuleNode } from 'vite'
+import { parse, HTMLElement } from 'node-html-parser'
 import type { VueHeadClient, MergeHead } from '@unhead/vue'
-import { basename } from 'node:path'
 import { State } from '../types'
-import { renderCssForSsr } from './renderCssForSsr'
-
-function renderPreloadLinks(modules: string[], manifest: any /* TODO */) {
-  let links = ''
-  const seen = new Set()
-
-  modules.forEach(id => {
-    const files = manifest[id]
-
-    if (files) {
-      files.forEach((file: any /* TODO */) => {
-        if (!seen.has(file)) {
-          seen.add(file)
-
-          const filename = basename(file)
-
-          if (manifest[filename]) {
-            for (const depFile of manifest[filename]) {
-              links += renderPreloadLink(depFile)
-              seen.add(depFile)
-            }
-          }
-
-          links += renderPreloadLink(file)
-        }
-      })
-    }
-  })
-
-  return links
-}
-
-function renderPreloadLink(file: string) {
-  if (file.endsWith('.js')) {
-    return `<link rel="modulepreload" crossorigin href="${file}">`
-  } else if (file.endsWith('.css')) {
-    return `<link rel="stylesheet" href="${file}">`
-  } else if (file.endsWith('.woff')) {
-    return ` <link rel="preload" href="${file}" as="font" type="font/woff" crossorigin>`
-  } else if (file.endsWith('.woff2')) {
-    return ` <link rel="preload" href="${file}" as="font" type="font/woff2" crossorigin>`
-  } else if (file.endsWith('.gif')) {
-    return ` <link rel="preload" href="${file}" as="image" type="image/gif">`
-  } else if (file.endsWith('.jpg') || file.endsWith('.jpeg')) {
-    return ` <link rel="preload" href="${file}" as="image" type="image/jpeg">`
-  } else if (file.endsWith('.png')) {
-    return ` <link rel="preload" href="${file}" as="image" type="image/png">`
-  } else {
-    // TODO
-    return ''
-  }
-}
 
 export async function generateHtml(template: string,
+                                   preloadLinks: string,
                                    rendered: string,
-                                   ctx: SSRContext,
+                                   teleports: Record<string, string>,
                                    state: State,
                                    head: VueHeadClient<MergeHead>,
-                                   cssModules?: Set<ModuleNode>,
-                                   manifest?: object) {
+                                   styles?: string) {
   const root = parse(template, { comment: true })
 
-  // $('#app').html(rendered)
   root.getElementById('app')?.set_content(rendered)
 
-  const preloadLinks = renderPreloadLinks(ctx.modules, manifest ?? {})
-  // $('head').append(preloadLinks)
-  root.querySelector('head')?.insertAdjacentHTML('beforeend', preloadLinks)
+  const htmlHead = root.querySelector('head')
 
-  if (cssModules !== undefined) {
-    const styles = renderCssForSsr(cssModules)
-    // $('head').append(styles)
-    root.querySelector('head')?.insertAdjacentHTML('beforeend', styles)
+  htmlHead?.insertAdjacentHTML('beforeend', preloadLinks)
+
+  if (styles !== undefined) {
+    htmlHead?.insertAdjacentHTML('beforeend', styles)
   }
 
   if (state.value !== undefined) {
     const { uneval } = await import('devalue')
 
-    // $('body').append(`<script>window.__INITIAL_STATE__ = ${uneval(state.value)}</script>`)
-    root.querySelector('head')?.insertAdjacentHTML('afterend', `<script>window.__INITIAL_STATE__ = ${uneval(state.value)}</script>`)
+    htmlHead?.insertAdjacentHTML('afterend', `<script>window.__INITIAL_STATE__ = ${uneval(state.value)}</script>`)
   }
 
-  const teleports = ctx.teleports ?? {}
+  const body = root.querySelector('body')
 
   if (teleports['#teleports'] !== undefined) {
-    // $('body').append(`<div id="teleports">${teleports['#teleports']}</div>`)
-    root.querySelector('body')?.insertAdjacentHTML('afterend', `<div id="teleports">${teleports['#teleports']}</div>`)
+    body?.insertAdjacentHTML('afterend', `<div id="teleports">${teleports['#teleports']}</div>`)
   }
 
-  // const resolvedTags = await head.resolveTags()
+  const resolvedTags = await head.resolveTags()
 
-  // let tags = ['title', 'meta', 'link', 'base', 'style', 'script', 'noscript']
+  const htmlTitle = root.querySelector('title')
 
-  // if ($('title').length === 1) {
-  //   tags = tags.filter(t => t !== 'title')
-  //   const title = resolvedTags.find(t => t.tag === 'title')
+  if (htmlTitle !== null) {
+    const title = resolvedTags.find(t => t.tag === 'title')
 
-  //   if (title !== undefined) {
-  //     // @ts-ignore
-  //     $('title').text(title.textContent)
-  //   }
-  // }
+    if (title !== undefined) {
+      htmlTitle.textContent = title.textContent ?? ''
+    }
+  }
 
-  // tags.map(tag => {
-  //   resolvedTags
-  //     .filter(t => t.tag === tag)
-  //     .map(t => {
-  //       let props = ''
+  const allowedTags = ['meta', 'link', 'base', 'style', 'script', 'noscript']
 
-  //       for (const [key, value] of Object.entries(t.props)) {
-  //         props = `${props} ${key}="${value}"`
-  //       }
+  resolvedTags
+    .filter(tag => tag.tag === allowedTags.find(allowed => allowed === tag.tag))
+    .forEach(tag => {
+      let props = ''
 
-  //       if (t.innerHTML !== undefined) {
-  //         $('head').append(`<${tag} ${props}>${t.innerHTML}</${tag}>`)
-  //       } else {
-  //         $('head').append(`<${tag} ${props}>`)
-  //       }
-  //     })
-  // })
+      for (const [key, value] of Object.entries(tag.props)) {
+        props = `${props} ${key}="${value}"`
+      }
 
-  // const bodyAttrs = resolvedTags.find(t => t.tag === 'bodyAttrs')
+      const el = new HTMLElement(tag.tag, {}, props)
 
-  // if (bodyAttrs !== undefined) {
-  //   for (const [key, value] of Object.entries(bodyAttrs.props)) {
-  //     $('body').attr(key, value)
-  //   }
-  // }
+      if (tag.innerHTML !== undefined) {
+        el.textContent = tag.innerHTML
+      }
 
-  // const htmlAttrs = resolvedTags.find(t => t.tag === 'htmlAttrs')
+      htmlHead?.appendChild(el)
+    })
 
-  // if (htmlAttrs !== undefined) {
-  //   for (const [key, value] of Object.entries(htmlAttrs.props)) {
-  //     $('html').attr(key, value)
-  //   }
-  // }
+  const bodyAttrs = resolvedTags.find(t => t.tag === 'bodyAttrs')
+
+  if (bodyAttrs !== undefined) {
+    for (const [key, value] of Object.entries(bodyAttrs.props)) {
+      body?.setAttribute(key, value)
+    }
+  }
+
+  const htmlAttrs = resolvedTags.find(t => t.tag === 'htmlAttrs')
+  const htmlRoot = root.querySelector('html')
+
+  if (htmlAttrs !== undefined) {
+    for (const [key, value] of Object.entries(htmlAttrs.props)) {
+      htmlRoot?.setAttribute(key, value)
+    }
+  }
 
   return root.toString()
 }
